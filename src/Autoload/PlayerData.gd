ce9bcc61f68eb0af52b644fd23a1d0967dbb268c
Rita Malaquias <ana.rita.malaquias@live.com>
2023-02-01 00:04:55 +0000

    addon broke project, modified a lot of stuff, still need some fixes

--- src/Autoload/PlayerData.gd
+++ src/Autoload/PlayerData.gd
@@ -1,19 +1,23 @@
 extends Node
 
-signal score_updated
-signal player_died
+
+signal updated
+signal died
 
 var score: = 0 setget set_score
 var deaths: = 0 setget set_deaths
 
-func reset() -> void:
-	score = 0
-	deaths = 0
 
-func set_score(value: int) -> void:
-	score = value
-	emit_signal("score_updated")
-	
-func set_deaths(value: int) -> void:
-	deaths = value
-	emit_signal("player_died")
+func reset():
+	self.score = 0
+	self.deaths = 0
+
+
+func set_score(new_score: int) -> void:
+	score = new_score
+	emit_signal("updated")
+
+
+func set_deaths(new_value: int) -> void:
+	deaths = new_value
+	emit_signal("died")
--- src/Objects/Portal2D.gd
+++ src/Objects/Portal2D.gd
@@ -1,17 +1,21 @@
 tool
 extends Area2D
 
-onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 export var next_scene: PackedScene
 
-func _on_body_entered(body: Node) -> void:
+
+func _on_body_entered(body: PhysicsBody2D):
 	teleport()
 
+
 func _get_configuration_warning() -> String:
-	return "The next scene property can't be empty" if not next_scene else ""
+	return "The property Next Level can't be empty" if not next_scene else ""
+
 
 func teleport() -> void:
-	anim_player.play("fade_in")
+	anim_player.play("fade_out")
 	yield(anim_player, "animation_finished")
 	get_tree().change_scene_to(next_scene)
