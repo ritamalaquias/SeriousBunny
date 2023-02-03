0d77fb9d8ffde2a6e281a8c31dcbe0f8e7d6a374
Rita Malaquias <ana.rita.malaquias@live.com>
2023-02-03 19:20:09 +0000

    fixed time line end on dialog

--- src/Actors/NPC.gd
+++ src/Actors/NPC.gd
@@ -10,13 +10,13 @@ func _process(delta):
 	$QuestionMark.visible = active	
 
 func _input(event):
-	if get_node_or_null('DialogeNode') == null:
-		if event.is_action_pressed("ui_accept") and active:
-			get_tree().paused = true
-			var dialog = Dialogic.start("timeline-1")
-			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-			dialog.connect("timeline_end", self, "unpause")
-			add_child(dialog)
+	#if get_node_or_null('DialogNode') == null:
+	if event.is_action_pressed("ui_accept") and active:
+		get_tree().paused = true
+		var dialog = Dialogic.start("timeline-1")
+		dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+		dialog.connect("timeline_end", self, "unpause")
+		add_child(dialog)
 
 func unpause(timeline_name):
 	get_tree().paused = false
